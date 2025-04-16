e410ef11743cc8eaf2360c102e4d2a718d97220d
Luc <520351@student.fontys.nl>
2025-04-16 23:13:50 +0200

    adding tables creation

--- internal/db/connection.go
+++ internal/db/connection.go
@@ -3,14 +3,20 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
 func InitDB() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
@@ -18,7 +24,7 @@ func InitDB() {
 	name := os.Getenv("DB_NAME")
 	password := os.Getenv("DB_PASSWORD")
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, name)
+	connStr := fmt.Sprintf("host=%s user=%s port=%s dbname=%s password=%s  sslmode=disable", host, user, port, name, password)
 
 	var err error
 	DB, err = sql.Open("postgres", connStr)
@@ -27,6 +33,33 @@ func InitDB() {
 	}
 	// defer DB.Close()
 
-	fmt.Println("DB working")
-	// createTables()
+	createTables()
+}
+
+func createTables() {
+	_, err := DB.Exec(`CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		email VARCHAR(45),
+		password TEXT
+	);`)
+	if err != nil {
+		log.Fatalf("Could not create users table: %v", err)
+	}
+
+	_, err = DB.Exec(`CREATE TABLE IF NOT EXISTS todo (
+		id SERIAL PRIMARY KEY,
+		title VARCHAR(45),
+		description TEXT,
+		complete BOOLEAN,
+		priority VARCHAR(20),
+		category VARCHAR(20),
+		createdAt timestamp,
+		dueDate timestamp,
+		userID INTEGER,
+		FOREIGN KEY (userID) REFERENCES users(id)
+	);`)
+	if err != nil {
+		panic("Could not create todo table.")
+	}
+
 }
