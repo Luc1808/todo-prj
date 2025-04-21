b3431ff27224ee805b634e40d70fe653bc49ba83
Luc <520351@student.fontys.nl>
2025-04-21 21:54:43 +0200

    adjusting todo model

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -2,14 +2,30 @@ package models
 
 import "time"
 
+type Priorities string
+type Categories string
+
+const (
+	High   Priorities = "High"
+	Medium Priorities = "Medium"
+	Low    Priorities = "Low"
+)
+
+const (
+	Health  Categories = "Health"
+	SelfDev Categories = "Self Development"
+	Finance Categories = "Finance"
+	Social  Categories = "Social"
+)
+
 type Todo struct {
 	ID          string
-	Title       string    `json:"title"`
-	Description string    `json:"descrition"`
-	Complete    bool      `json:"complete"`
-	Priority    string    `json:"priority"`
-	Category    string    `json:"category"`
-	CreatedAt   time.Time `json:"createdAt"`
-	DueAt       time.Time `json:"dueAt"`
-	UserID      uint      `json:"userID"`
+	Title       string     `json:"title"`
+	Description string     `json:"descrition"`
+	Complete    bool       `json:"complete"`
+	Priority    Priorities `json:"priority"`
+	Category    Categories `json:"category"`
+	CreatedAt   time.Time  `json:"createdAt"`
+	DueAt       time.Time  `json:"dueAt"`
+	UserID      uint       `json:"userID"`
 }
